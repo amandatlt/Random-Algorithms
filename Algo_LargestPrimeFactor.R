b6947d254e375fed97f5e3dbc828d6ad1170ccc6
amandatlt <amanda.teo@nera.com>
2017-03-08 22:26:24 +0000

    Find largest prime factor
    Finds the larger prime factor of a given number

---
+++ Algo_LargestPrimeFactor.R
@@ -0,0 +1,33 @@
+#'
+#' algorithm for problem 3 of project euler
+#' finding the largest prime factor of a number
+#' solved with the basis that numbers are always product of primes
+#' @example the number 13195 should return 29
+#'
+
+find_prime <- function(x) {
+  i = floor(sqrt(x))
+  while (i > 1) {
+    result <- x/i
+    success <- ifelse(result - round(result) == 0, TRUE, FALSE)
+    if (success == TRUE) {
+      x <- find_prime(result)
+      break
+    } else {
+      i = i - 1
+    }
+  }
+  return(x)
+}
+
+largest_prime <- function(x) {
+  V[1] <- x
+  i = 1
+  while (x > 1) {
+    y <- find_prime(x)
+    V[i] <- y
+    x <- x/y
+    i = i + 1
+  }
+  return(max(V))
+}
